661459bf6a1f265df03f026e2f62fa22670e4c7b
kailaniwoodard <woodardk@allegheny.edu>
2022-01-04 23:24:03 -0500

    Continued importing Greyscale modified to index.js

--- curatrix/pages/index.js
+++ curatrix/pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
               <div className="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
                   <div className="d-flex justify-content-center">
                       <div className="text-center">
-                          <h1 className="mx-auto my-0 text-uppercase">Curatrix</h1>
+                          <h1 className="text-white mx-auto my-0 text-uppercase">Curatrix</h1>
                           <img className="img-center img-resize-logo" src="/img/for_Spotify.png" alt="for Spotify" />
                           <h6 className="text-white-50 mx-auto mt-2 mb-5 fw-lighter fst-italic">created by lazy listeners, for lazy listeners</h6>
                           <a className="btn btn-primary" href="#about">Get Started</a>
@@ -54,6 +54,62 @@ export default function Home() {
                   </div>
               </div>
           </header>
+          <section class="about-section text-center" id="about">
+              <div class="container px-4 px-lg-5">
+                  <div class="row gx-4 gx-lg-5 justify-content-center">
+                      <div class="col-lg-8">
+                          <h2 class="text-white mb-4">Built with Bootstrap 5</h2>
+                          <p class="text-white-50">
+                              Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template
+                              on <a href="https://startbootstrap.com/theme/grayscale/">the preview page.</a> The
+                              theme is open source, and you can use it for any purpose, personal or commercial.
+                          </p>
+                      </div>
+                  </div>
+                  <img class="img-fluid" src="img/ipad.png" alt="..." />
+              </div>
+          </section>
+          <section class="projects-section bg-light" id="projects">
+              <div class="container px-4 px-lg-5">
+                  <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                      <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt="..." /></div>
+                      <div class="col-xl-4 col-lg-5">
+                          <div class="featured-text text-center text-lg-left">
+                              <h4>Shoreline</h4>
+                              <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                      <div class="col-lg-6"><img class="img-fluid" src="img/demo-image-01.jpg" alt="..." /></div>
+                      <div class="col-lg-6">
+                          <div class="bg-black text-center h-100 project">
+                              <div class="d-flex h-100">
+                                  <div class="project-text w-100 my-auto text-center text-lg-left">
+                                      <h4 class="text-white">Misty</h4>
+                                      <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                                      <hr class="d-none d-lg-block mb-0 ms-0" />
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="row gx-0 justify-content-center">
+                      <div class="col-lg-6"><img class="img-fluid" src="img/demo-image-02.jpg" alt="..." /></div>
+                      <div class="col-lg-6 order-lg-first">
+                          <div class="bg-black text-center h-100 project">
+                              <div class="d-flex h-100">
+                                  <div class="project-text w-100 my-auto text-center text-lg-right">
+                                      <h4 class="text-white">Mountains</h4>
+                                      <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                                      <hr class="d-none d-lg-block mb-0 me-0" />
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </section>
         </body>
       </main>
     </div>
