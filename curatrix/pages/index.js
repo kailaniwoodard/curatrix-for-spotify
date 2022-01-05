8293f0870fb1c22779e1ee8d34aeb95e0f090d30
kailaniwoodard <woodardk@allegheny.edu>
2022-01-04 23:36:57 -0500

    Completed (mostly) import of modified Greyscale

--- curatrix/pages/index.js
+++ curatrix/pages/index.js
@@ -54,64 +54,115 @@ export default function Home() {
                   </div>
               </div>
           </header>
-          <section class="about-section text-center" id="about">
-              <div class="container px-4 px-lg-5">
-                  <div class="row gx-4 gx-lg-5 justify-content-center">
-                      <div class="col-lg-8">
-                          <h2 class="text-white mb-4">Built with Bootstrap 5</h2>
-                          <p class="text-white-50">
+          <section className="about-section text-center" id="about">
+              <div className="container px-4 px-lg-5">
+                  <div className="row gx-4 gx-lg-5 justify-content-center">
+                      <div className="col-lg-8">
+                          <h2 className="text-white mb-4">Built with Bootstrap 5</h2>
+                          <p className="text-white-50">
                               Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template
                               on <a href="https://startbootstrap.com/theme/grayscale/">the preview page.</a> The
                               theme is open source, and you can use it for any purpose, personal or commercial.
                           </p>
                       </div>
                   </div>
-                  <img class="img-fluid" src="img/ipad.png" alt="..." />
+                  <img className="img-fluid" src="img/ipad.png" alt="..." />
               </div>
           </section>
-          <section class="projects-section bg-light" id="projects">
-              <div class="container px-4 px-lg-5">
-                  <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                      <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt="..." /></div>
-                      <div class="col-xl-4 col-lg-5">
-                          <div class="featured-text text-center text-lg-left">
+          <section className="projects-section bg-light" id="projects">
+              <div className="container px-4 px-lg-5">
+                  <div className="row gx-0 mb-4 mb-lg-5 align-items-center">
+                      <div className="col-xl-8 col-lg-7"><img className="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt="..." /></div>
+                      <div className="col-xl-4 col-lg-5">
+                          <div className="featured-text text-center text-lg-left">
                               <h4>Shoreline</h4>
-                              <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                              <p className="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
                           </div>
                       </div>
                   </div>
-                  <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-                      <div class="col-lg-6"><img class="img-fluid" src="img/demo-image-01.jpg" alt="..." /></div>
-                      <div class="col-lg-6">
-                          <div class="bg-black text-center h-100 project">
-                              <div class="d-flex h-100">
-                                  <div class="project-text w-100 my-auto text-center text-lg-left">
-                                      <h4 class="text-white">Misty</h4>
-                                      <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
-                                      <hr class="d-none d-lg-block mb-0 ms-0" />
+                  <div className="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                      <div className="col-lg-6"><img className="img-fluid" src="img/demo-image-01.jpg" alt="..." /></div>
+                      <div className="col-lg-6">
+                          <div className="bg-black text-center h-100 project">
+                              <div className="d-flex h-100">
+                                  <div className="project-text w-100 my-auto text-center text-lg-left">
+                                      <h4 className="text-white">Misty</h4>
+                                      <p className="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                                      <hr className="d-none d-lg-block mb-0 ms-0" />
                                   </div>
                               </div>
                           </div>
                       </div>
                   </div>
-                  <div class="row gx-0 justify-content-center">
-                      <div class="col-lg-6"><img class="img-fluid" src="img/demo-image-02.jpg" alt="..." /></div>
-                      <div class="col-lg-6 order-lg-first">
-                          <div class="bg-black text-center h-100 project">
-                              <div class="d-flex h-100">
-                                  <div class="project-text w-100 my-auto text-center text-lg-right">
-                                      <h4 class="text-white">Mountains</h4>
-                                      <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
-                                      <hr class="d-none d-lg-block mb-0 me-0" />
+                  <div className="row gx-0 justify-content-center">
+                      <div className="col-lg-6"><img className="img-fluid" src="img/demo-image-02.jpg" alt="..." /></div>
+                      <div className="col-lg-6 order-lg-first">
+                          <div className="bg-black text-center h-100 project">
+                              <div className="d-flex h-100">
+                                  <div className="project-text w-100 my-auto text-center text-lg-right">
+                                      <h4 className="text-white">Mountains</h4>
+                                      <p className="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                                      <hr className="d-none d-lg-block mb-0 me-0" />
                                   </div>
                               </div>
                           </div>
                       </div>
+                  </div>
+              </div>
+          </section>
+          <section className="contact-section bg-black">
+              <div className="container px-4 px-lg-5">
+                  <div className="row gx-4 gx-lg-5">
+                      <div className="col-md-4 mb-3 mb-md-0">
+                          <div className="card py-4 h-100">
+                              <div className="card-body text-center">
+                                  <i className="fab fa-spotify text-primary mb-2"></i>
+                                  <h4 className="text-uppercase m-0">Spotify</h4>
+                                  <hr className="my-4 mx-auto" />
+                                  <div className="small text-black-50"><a href="https://open.spotify.com/user/kai.lani">kai.lani</a></div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="col-md-4 mb-3 mb-md-0">
+                          <div className="card py-4 h-100">
+                              <div className="card-body text-center">
+                                  <i className="fas fa-envelope text-primary mb-2"></i>
+                                  <h4 className="text-uppercase m-0">Email</h4>
+                                  <hr className="my-4 mx-auto" />
+                                  <div className="small text-black-50">
+                                    <div className="text-decoration-none">
+                                      <a href="mailto:kailanimila@gmail.com">kailanimila@gmail.com</a>
+                                    </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="col-md-4 mb-3 mb-md-0">
+                          <div className="card py-4 h-100">
+                              <div className="card-body text-center">
+                                  <i className="fas fa-code text-primary mb-2"></i>
+                                  <h4 className="text-uppercase m-0">Source Code</h4>
+                                  <hr className="my-4 mx-auto" />
+                                  <div className="small text-black-50">
+                                    <a href="!#">Curatrix for Spotify</a>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="social d-flex justify-content-center">
+                      <a className="mx-2" href="https://www.instagram.com/kai.lani/"><i className="fab fa-instagram"></i></a>
+                      <a className="mx-2" href="https://www.linkedin.com/in/kailani-woodard/"><i className="fab fa-linkedin"></i></a>
+                      <a className="mx-2" href="https://github.com/kailaniwoodard"><i className="fab fa-github"></i></a>
                   </div>
               </div>
           </section>
         </body>
       </main>
+      <footer className="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Curatrix for Spotify 2022</div></footer>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="js/scripts.js"></script>
+      <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
     </div>
   )
 }
