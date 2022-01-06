6b924929cd896520a9f7a6c7e4c18d27fc659adb
kailaniwoodard <woodardk@allegheny.edu>
2022-01-05 20:55:38 -0500

    Working functionality, uri errors

--- curatrix/pages/api/auth/[...nextauth].js
+++ curatrix/pages/api/auth/[...nextauth].js
@@ -12,14 +12,14 @@ const scope = ['user-read-email',
                 'playlist-modify-public'
               ].join(" ");
 
+const authLink = 'https://accounts.spotify.com/authorize?client_id=a0de4f15bb4348d18941c34b335bafc0&scope=user-read-email%20user-read-private%20user-library-read%20playlist-modify-private%20playlist-read-collaborative%20playlist-read-private%20user-top-read%20playlist-modify-public&response_type=code&redirect_uri=http%3A%2F%2Flocalhost:3000%2Fapi%2Fauth%2Fcallback%2Fspotify%2Fcallback%2Fspotify&state=xUKxDA9J7xE6piWCBGlwE8I0Et7iXhJzllSN-hPYvRs'
+
 export default NextAuth({
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      authorization: {
-        params: { scope },
-      },
+      authorization: { authLink },
     }),
   ],
   secret: process.env.JWT_SECRET,
