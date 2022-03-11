65b8a9c0464c107865ea04068779752c1b669777
kailaniwoodard <woodardk@allegheny.edu>
2022-03-11 11:20:55 -0500

    Cookie tweaks

--- curatrix/utils/cookies.ts
+++ curatrix/utils/cookies.ts
@@ -11,11 +11,7 @@ export interface UserSession {
     id: string
     display_name: string
     email: string
-    images: {
-      width: number
-      height: number
-      url: string
-    }[]
+    image_url: string
   }
   token: {
     access_token: string
@@ -25,8 +21,6 @@ export interface UserSession {
     scope: string
   }
 }
-
-//
 export type ApiRequestWithToken = NextApiRequest & {
   session: UserSession
 }
@@ -44,7 +38,6 @@ export const setAuthCookie = async (
     sameSite: 'strict',
     path: '/',
   }
-
   const opts: CookieSerializeOptions = { ...defaults, ...options }
 
   try {
@@ -65,7 +58,7 @@ export const setAuthCookie = async (
       opts.maxAge /= 1000
     }
 
-    res.setHeader('Set-Cookie', serialize('auth.session', stringValue, opts))
+    res.setHeader('Set-Cookie', serialize('user.session', stringValue, opts))
   } catch (error) {
     console.error('Failed to seal session object', error)
     return
@@ -75,24 +68,41 @@ export const setAuthCookie = async (
 export const getSessionCookie = async (
   cookies: Record<string, string>
 ): Promise<UserSession> => {
-  if (!cookies['auth.session']) {
-    throw new Error('Authenticated session not found!')
+  if (!cookies['user.session']) {
+    throw new Error('Session not found!')
   }
 
-  const cookie = cookies['auth.session']
+  const cookie = cookies['user.session']
   const decoded = await Iron.unseal(cookie, SESSION_SECRET, Iron.defaults)
 
   return decoded
 }
 
-export const encodeAuth = (id: string, secret: string) => {
-  return Buffer.from(`${id}:${secret}`).toString('base64')
+export const getAuthToken = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const session = await getSessionCookie(req.cookies)
+    return session.token.access_token
+  } catch {
+    throw new Error('Auth token not found!')
+  }
 }
 
-export const handleAuthTokenExpiration = (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {}
+export const withAuthSession = fn => async (
+  req: ApiRequestWithToken,
+  res: NextApiResponse,
+) => {
+  try {
+    const session = (await getSessionCookie(req.cookies)) as UserSession
+    req.session = session
+
+    return await fn(req, res)
+  } catch (error) {
+    return res.status(401).json({
+      statusCode: 401,
+      message: 'Unauthorized'
+    })
+  }
+}
 
 export const refreshAuthToken = (refreshToken: string) => {
   return axios.post(
@@ -101,7 +111,7 @@ export const refreshAuthToken = (refreshToken: string) => {
       client_id: CLIENT_ID,
       client_secret: CLIENT_SECRET,
       grant_type: 'refresh_token',
-      refreshToken: refreshToken
+      refreshToken: refreshToken,
     }),
     {
       headers: {
